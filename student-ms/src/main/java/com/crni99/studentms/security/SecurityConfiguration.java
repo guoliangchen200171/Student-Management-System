2f51d6c6c761150f37fbf2405d1f5c92f80d9f44
guoliangchen200171 <144734248+guoliangchen200171@users.noreply.github.com>
2026-05-16 20:48:52 +0800

    address auth

--- student-ms/src/main/java/com/crni99/studentms/security/SecurityConfiguration.java
+++ student-ms/src/main/java/com/crni99/studentms/security/SecurityConfiguration.java
@@ -11,8 +11,14 @@ public class SecurityConfiguration {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+		http.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/api/**").permitAll()
+				.requestMatchers("/h2-console/**").permitAll()
+				.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+				.anyRequest().authenticated());
 		http.httpBasic(withDefaults());
+		http.csrf(csrf -> csrf.ignoringRequestMatchers("/api/**", "/h2-console/**"));
+		http.headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()));
 		return http.build();
 	}
 
